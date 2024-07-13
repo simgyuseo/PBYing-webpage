fa3fa1f87977954a016c06a8347b8f9b5a0bf403
simgyuseo <128901959+simgyuseo@users.noreply.github.com>
2024-07-13 14:37:16 +0900

    Add files via upload
    commit2

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+
+export default function App() {
+  return (
+    <WebView
+    
+      source={{ uri: 'https://pbyinghomepage.netlify.app/' }}
+    />
+  );
+}
